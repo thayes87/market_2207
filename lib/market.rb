9df7ea8103f5163f8572a878b455011fee952adc
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-08 10:16:47 -0600

    add vendors_that_sell method

--- lib/market.rb
+++ lib/market.rb
@@ -1,2 +1,29 @@
 class Market
+  attr_reader :name, :vendors
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    vendors.map do |vendor|
+      vendor.name 
+    end 
+  end
+
+  def vendors_that_sell(product)
+    vendor_list = []
+    vendors.each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        if item.name == product.name
+          vendor_list << vendor
+        end
+      end
+    end
+    vendor_list
+  end
 end
---
+++ spec/market_spec.rb
@@ -0,0 +1,55 @@
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+RSpec.describe Market do
+  let(:market) { Market.new("South Pearl Street Farmers Market") }
+  let(:vendor1) { Vendor.new("Rocky Mountain Fresh") }
+  let(:vendor2) { Vendor.new("Ba-Nom-a-Nom") }
+  let(:vendor3) { Vendor.new("Palisade Peach Shack") }
+  let(:item1) { Item.new({name: 'Peach', price: "$0.75"}) }
+  let(:item2) { Item.new({name: 'Tomato', price: '$0.50'}) }
+  let(:item3) { Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"}) }
+  let(:item4) { Item.new({name: "Banana Nice Cream", price: "$4.25"}) }
+
+  it 'exists with attributes' do
+
+    expect(market).to be_instance_of(Market)
+    expect(market.name).to eq("South Pearl Street Farmers Market")
+    expect(market.vendors).to eq([])
+  end
+
+  it 'can add vendors to the market' do
+   
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    expect(market.vendors).to eq([vendor1, vendor2, vendor3])
+  end
+
+  it 'can pull a list of vendors by name' do
+    
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    expect(market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+  end
+
+  it 'can return a list of vendors that sell a given item that is in stock' do
+    
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    expect(market.vendors_that_sell(item1)).to eq([vendor1, vendor3])
+    expect(market.vendors_that_sell(item4)).to eq([vendor2])
+  end
+end 
